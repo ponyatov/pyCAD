95dbd9b9e79f64d25a0fe3a5f3c1bdb802ba64c1
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-26 00:30:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/pycad/py.py
+++ src/pycad/py.py
@@ -6,8 +6,8 @@ class PyLexer(syntax.Lexer):
     t_COMMENT = r'\#[^\r\n]*'
     t_KEYWORD = r'\b(and|as|assert|break|class|continue|def|del|elif|else|except|False|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|not|or|pass|raise|return|True|try|while|with|yield)\b'
     t_STRING = r'\"([^\\\n]|(\\.))*?\"|\'([^\\\n]|(\\.))*?\''
-    t_QT = r'PyQt\d|Qt?[A-Z][A-Za-z]*'
     t_STDLIB = r'\b(os|sys|print|__name__|code|traceback)\b'
+    t_QT = r'PyQt\d|Qt?[A-Z][A-Za-z]*'
 
 # from PyQt6.QtGui import *
 
@@ -21,6 +21,3 @@ class Highlighter(syntax.Highlighter):
         match tok_type:
             case 'QT': return QT
             case _: return super().format(tok_type)
-
-    # case 'QT':
-    #     self.setFormat(start, length, QT())
