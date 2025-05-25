69d5d5714d28961273357c68001879a969cc9733
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-26 00:51:17 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/pycad/file.py
+++ src/pycad/file.py
@@ -6,14 +6,18 @@ import colors
 class ColoredFileModel(QFileSystemModel):
 
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
-        """Override data method to apply coloring"""
+
         if role == Qt.ItemDataRole.ForegroundRole:
             file_info = QFileInfo(self.filePath(index))
 
             if file_info.isDir():
                 return QBrush(QColor(colors.DIR.color))
 
-            match file_info.suffix().lower():
-                case _: return QBrush(QColor(colors.FORMAT.color))
+            def ext2color(file_info):
+                match file_info.suffix().lower():
+                    case 'py': return colors.PY
+                    case _: return colors.FORMAT
+
+            return QBrush(QColor(ext2color(file_info).color))
 
         return super().data(index, role)
